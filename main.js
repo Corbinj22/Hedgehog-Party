077e879bc7d52f7b8f229e5004cecb7a0490b40f
Justin Corbin <jcorbin2290@gmail.com>
2020-01-22 12:19:06 -0700

    JS variables for document added

---
+++ main.js
@@ -0,0 +1,4 @@
+var headgehogDetails = document.querySelectorAll('.headgehog-details');
+var inviteBtn = document.querySelector('.invite-btn');
+var uninviteBtn = document.querySelector('.uninvite-btn');
+var guestList = document.getElementById('guest-table');
